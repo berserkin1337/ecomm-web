31b821fe84946f4c5a51d701fa47c3ce020c5abf
berserkin1337 <gaviral373@gmail.com>
2023-05-26 15:25:17 +0530

    Turn the scss into styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,95 +1,81 @@
-import { useState} from 'react';
+import {useState} from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
+import {signInAuthUserWithEmailAndPassword, signInWithGooglePopup,} from '../../utils/firebase/firebase.utils';
 
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInAuthUserWithEmailAndPassword,
-} from '../../utils/firebase/firebase.utils';
-
-import './sign-in-form.styles.scss';
+import {ButtonsContainer, SignInContainer} from './sign-in-form.styles';
 
 const defaultFormFields = {
-  email: '',
-  password: '',
+    email: '',
+    password: '',
 };
 
 const SignInForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { email, password } = formFields;
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {email, password} = formFields;
 
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
 
-  const signInWithGoogle = async () => {
-    await signInWithGooglePopup();
-  };
+    const signInWithGoogle = async () => {
+        await signInWithGooglePopup();
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    try {
-      const {user} = await signInAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
+        try {
+            await signInAuthUserWithEmailAndPassword(email, password);
+            resetFormFields();
+        } catch (error) {
+            console.log('user sign in failed', error);
+        }
+    };
 
-      resetFormFields();
-    } catch (error) {
-      switch (error.code) {
-        case 'auth/wrong-password':
-          alert('incorrect password for email');
-          break;
-        case 'auth/user-not-found':
-          alert('no user associated with this email');
-          break;
-        default:
-          console.log(error);
-      }
-    }
-  };
+    const handleChange = (event) => {
+        const {name, value} = event.target;
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+        setFormFields({...formFields, [name]: value});
+    };
 
-    setFormFields({ ...formFields, [name]: value });
-  };
+    return (
+        <SignInContainer>
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label='Email'
+                    type='email'
+                    required
+                    onChange={handleChange}
+                    name='email'
+                    value={email}
+                />
 
-  return (
-    <div className='sign-up-container'>
-      <h2>Already have an account?</h2>
-      <span>Sign in with your email and password</span>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-          label='Email'
-          type='email'
-          required
-          onChange={handleChange}
-          name='email'
-          value={email}
-        />
-
-        <FormInput
-          label='Password'
-          type='password'
-          required
-          onChange={handleChange}
-          name='password'
-          value={password}
-        />
-        <div className='buttons-container'>
-          <Button type='submit'>Sign In</Button>
-          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
-            Google sign in
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+                <FormInput
+                    label='Password'
+                    type='password'
+                    required
+                    onChange={handleChange}
+                    name='password'
+                    value={password}
+                />
+                <ButtonsContainer>
+                    <Button type='submit'>Sign In</Button>
+                    <Button
+                        buttonType={BUTTON_TYPE_CLASSES.google}
+                        type='button'
+                        onClick={signInWithGoogle}
+                    >
+                        Sign In With Google
+                    </Button>
+                </ButtonsContainer>
+            </form>
+        </SignInContainer>
+    );
 };
 
 export default SignInForm;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,36 +1,44 @@
-import {Link, Outlet} from "react-router-dom";
-import {Fragment, useContext} from "react";
-import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
-import "./navigation.styles.scss";
-import {UserContext} from "../../contexts/user.context";
-import {signOutUser} from "../../utils/firebase/firebase.utils";
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import {CartContext} from "../../contexts/cart.context";
+import {Fragment, useContext} from 'react';
+import {Outlet} from 'react-router-dom';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import {UserContext} from '../../contexts/user.context';
+import {CartContext} from '../../contexts/cart.context';
+
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import {signOutUser} from '../../utils/firebase/firebase.utils';
+
+import {LogoContainer, NavigationContainer, NavLink, NavLinks,} from './navigation.styles';
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
-    // console.log(currentUser);
     const {isCartOpen} = useContext(CartContext);
-    return (<Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to="/">
-                <CrwnLogo className="logo"/>
-            </Link>
-            <div className="nav-links-container">
-                <Link className="nav-link" to="/shop">
-                    SHOP
-                </Link>
-                {currentUser ? (<span className={"nav-link"} onClick={signOutUser}>
-              SIGN OUT
-            </span>) : (<Link className="sign-in" to="/auth">
-                    SIGN IN
-                </Link>)}
-                <CartIcon/>
-            </div>
-            {isCartOpen && <CartDropdown/>}
-        </div>
-        <Outlet/>
-    </Fragment>);
+
+    return (
+        <Fragment>
+            <NavigationContainer>
+                <LogoContainer to='/'>
+                    <CrwnLogo/>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to='/shop'>SHOP</NavLink>
+
+                    {currentUser ? (
+                        <NavLink as='span' onClick={signOutUser}>
+                            SIGN OUT
+                        </NavLink>
+                    ) : (
+                        <NavLink to='/auth'>SIGN IN</NavLink>
+                    )}
+                    <CartIcon/>
+                </NavLinks>
+                {isCartOpen && <CartDropdown/>}
+            </NavigationContainer>
+            <Outlet/>
+        </Fragment>
+    );
 };
+
 export default Navigation;
